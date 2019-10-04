8038c4581b37d220dbc67012d40cdcce7b8c99d2
RyomaTanaka <tanakaryoma1992@gmail.com>
2019-10-04 21:01:37 +0900

    chat-space-recover

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'messages#index'
-  get 'users/edit'
-  get 'users/update'
-  resources :users, only: [:edit, :update]
+  resources :users, only: [:new, :create, :edit, :update] do
+    resources :messages, only: [:create, :index]
+  end
 end
