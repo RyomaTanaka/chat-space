7b35d4075102550addd12dd9b69b91f53c27e35a
RyomaTanaka <tanakaryoma1992@gmail.com>
2019-10-11 16:46:32 +0900

    chat-sapce-end-3

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,4 +1,6 @@
 class Api::MessagesController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  
   def index
     group = Group.find(params[:group_id])
     last_message_id = params[:id].to_i
